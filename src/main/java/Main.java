5ee1fe25942d70a09ee0cc188f0977bc3ff5b8fa
Diego Ribeiro <drchaves@inf.ufsm.br>
2024-05-02 19:39:46 -0300

    superclasse de conexao (arrumada)

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ConexaoBanco conexaoBanco = new ConexaoBanco();
+        ManipuladorAlunos manipuladorAlunos = new ManipuladorAlunos();
+
+    }
+}
